05cba88305987e636ceeda1e8a20db4b5d5970fb
Gustavo Mex <gustavoppymex@gmail.com>
2022-02-10 19:13:23 -0600

    CREATION OF POST SERIALIZER

--- config/settings.py
+++ config/settings.py
@@ -39,8 +39,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # 3rd-party apps
+    'rest_framework',
+
     # Local
-    'posts'
+    'posts',
+
 ]
 
 MIDDLEWARE = [
@@ -125,3 +129,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny'
+    ]
+}
